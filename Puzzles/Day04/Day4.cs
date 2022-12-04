ea61310c4c89b5fef6d01f75ca656a6e1d8bcc28
MadLibberator <libbylord@hotmail.com>
2022-12-03 23:29:50 -0700

    Tidied up

--- Puzzles/Day04/Day4.cs
+++ Puzzles/Day04/Day4.cs
@@ -10,12 +10,9 @@ public class Day4 : Puzzle
     public Day4(ILogger logger, string path) : base(logger, path) { }
 
     public override void Setup() => _data = Utils.ReadFrom(_path).Select(line => line.Split('-', ',').ConvertToInts()).ToList();
-
-    public override void SolvePart1() => _logger.Log(_data.Count(sets => FullyContains(sets[..2], sets[2..])));
-
-    public override void SolvePart2() => _logger.Log(_data.Count(sets => PartiallyOverlaps(sets[..2], sets[2..])));
-
-    private static bool FullyContains(int[] a, int[] b) => (a[0] <= b[0] && a[1] >= b[1]) || (b[0] <= a[0] && b[1] >= a[1]);
-
-    private static bool PartiallyOverlaps(int[] a, int[] b) => a[0] <= b[1] && a[1] >= b[0];
+    public override void SolvePart1() => _logger.Log(_data.Count(pairs => FullyContains(pairs[..2], pairs[2..])));
+    public override void SolvePart2() => _logger.Log(_data.Count(pairs => PartiallyOverlaps(pairs[..2], pairs[2..])));
+    
+    private static bool FullyContains(int[] a, int[] b) => (a[0] <= b[0] && b[1] <= a[1]) || (b[0] <= a[0] && a[1] <= b[1]);
+    private static bool PartiallyOverlaps(int[] a, int[] b) => a[0] <= b[1] && b[0] <= a[1];
 }
