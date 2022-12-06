c9443318500f58a9a66a8bf4e99987be77b14509
MadLibberator <libbylord@hotmail.com>
2022-12-05 22:31:07 -0700

    Day 6 Complete: Tuning Trouble - Distinct sequence in string

--- Puzzles/Day06/Day6.cs
+++ Puzzles/Day06/Day6.cs
@@ -1,22 +1,25 @@
-﻿namespace AoC22;
+﻿using System;
+using System.Linq;
+
+namespace AoC22;
 
 public class Day6 : Puzzle
 {
+    private string _data = string.Empty;
+
     public Day6(ILogger logger, string path) : base(logger, path) { }
 
-    public override void Setup()
-    { 
-        // For parsing and/or caching shared data.
-        // Access file with ReadFromFile() for 1 line at a time or ReadAllLines() as a string dump
-    }
+    public override void Setup() => _data = ReadAllLines().Single();
 
-    public override void SolvePart1()
-    {
-        _logger.Log("Part 1 Answer");
-    }
+    public override void SolvePart1() => _logger.Log(IndexOfMessageMarker(_data, 4));
 
-    public override void SolvePart2()
+    public override void SolvePart2() => _logger.Log(IndexOfMessageMarker(_data, 14));
+
+    private static int IndexOfMessageMarker(string message, int distinctLength)
     {
-        _logger.Log("Part 2 Answer");
+        for (int i = 0; i < message.Length - distinctLength; i++)
+            if (message.Substring(i, distinctLength).Distinct().Count() == distinctLength)
+                return i + distinctLength;
+        throw new Exception($"Unique sequence of length {distinctLength} not found.");
     }
 }
