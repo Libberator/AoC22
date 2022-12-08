59a16f6aa3a1906385de523cf62d87681ab46282
MadLibberator <libbylord@hotmail.com>
2022-12-07 21:48:15 -0700

    Ready for Day8

--- Puzzles/Day08/Day8.cs
+++ Puzzles/Day08/Day8.cs
@@ -3,9 +3,9 @@
 public class Day8 : Puzzle
 {
     public Day8(ILogger logger, string path) : base(logger, path) { }
-
+    
     public override void Setup()
-    { 
+    {
         // Access file with ReadFromFile() for 1 line at a time or ReadAllLines() as a string dump
     }
 
