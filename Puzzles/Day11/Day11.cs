da8e7cdb4a290179728715ccc2e699d05275d6bf
MadLibberator <libbylord@hotmail.com>
2022-12-11 20:48:44 -0700

    Cleaned up Day 11

--- Puzzles/Day11/Day11.cs
+++ Puzzles/Day11/Day11.cs
@@ -7,24 +7,22 @@ namespace AoC22;
 
 public partial class Day11 : Puzzle
 {
-    // item starting values and positions (which monkey is holding them) - gets copied
+    private readonly List<Monkey> _monkeys = new();
+    // item starting values and their holder (which monkey ID is holding them) - these two get copied
     private readonly List<long> _itemStartingValues = new();
     private readonly List<byte> _itemStartingHolders = new();
-    private readonly List<Monkey> _monkeys = new();
 
     public Day11(ILogger logger, string path) : base(logger, path) { }
 
     public override void Setup()
     {
-        byte monkeyID = 0;
         var pattern = NumberPattern();
         Monkey? monkey = null;
         foreach (var line in ReadFromFile(ignoreWhiteSpace: true))
         {
             if (line.Contains("Monkey"))
             {
-                monkeyID = byte.Parse(pattern.Match(line).Groups[0].ValueSpan);
-                monkey = new Monkey(monkeyID);
+                monkey = new Monkey(byte.Parse(pattern.Match(line).ValueSpan));
                 _monkeys.Add(monkey);
             }
             else if (line.Contains("Starting items"))
@@ -32,8 +30,8 @@ public partial class Day11 : Puzzle
                 var matches = pattern.Matches(line);
                 for (int i = 0; i < matches.Count; i++)
                 {
-                    _itemStartingValues.Add(int.Parse(matches[i].Groups[0].ValueSpan));
-                    _itemStartingHolders.Add(monkeyID);
+                    _itemStartingValues.Add(int.Parse(matches[i].ValueSpan));
+                    _itemStartingHolders.Add(monkey!.Id);
                 }
             }
             else if (line.Contains("Operation"))
@@ -42,14 +40,14 @@ public partial class Day11 : Puzzle
                 if (!match.Success) monkey!.Operation = old => old * old;
                 else
                 {
-                    var value = int.Parse(match.Groups[0].ValueSpan);
+                    var value = int.Parse(match.ValueSpan);
                     if (line.Contains('*')) monkey!.Operation = old => old * value;
                     else monkey!.Operation = old => old + value; // line.Contains('+')
                 }
             }
             else
             {
-                var value = byte.Parse(pattern.Match(line).Groups[0].ValueSpan);
+                var value = byte.Parse(pattern.Match(line).ValueSpan);
                 if (line.Contains("Test")) monkey!.DivisibleByValue = value;
                 else if (line.Contains("If true")) monkey!.TrueTarget = value;
                 else if (line.Contains("If false")) monkey!.FalseTarget = value;
@@ -59,14 +57,11 @@ public partial class Day11 : Puzzle
 
     public override void SolvePart1()
     {
-        // copy over item values and initial positions to not affect the original
         var items = new List<long>(_itemStartingValues); 
         var itemHolders = new List<byte>(_itemStartingHolders);
-        
         var inspectionTotals = new long[_monkeys.Count];
-        Func<long, long> postInspection = worry => worry / 3;
 
-        RunRounds(20, items, itemHolders, inspectionTotals, postInspection);
+        RunRounds(20, items, itemHolders, inspectionTotals, worry => worry / 3);
 
         var monkeyBusiness = inspectionTotals.OrderDescending().Take(2).Product();
         _logger.Log(monkeyBusiness);
@@ -74,15 +69,12 @@ public partial class Day11 : Puzzle
 
     public override void SolvePart2()
     {
-        // copy over item values and initial positions to not affect the original
         var items = new List<long>(_itemStartingValues);
         var itemHolders = new List<byte>(_itemStartingHolders);
-        
         var inspectionTotals = new long[_monkeys.Count];
         var greatestCommonDivisor = _monkeys.Select(m => m.DivisibleByValue).Product();
-        Func<long, long> postInspection = worry => worry % greatestCommonDivisor;
 
-        RunRounds(10_000, items, itemHolders, inspectionTotals, postInspection);
+        RunRounds(10_000, items, itemHolders, inspectionTotals, worry => worry % greatestCommonDivisor);
 
         var monkeyBusiness = inspectionTotals.OrderDescending().Take(2).Product();
         _logger.Log(monkeyBusiness);
