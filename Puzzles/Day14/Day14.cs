42a10e8a98969309d2cd6f79065012bec2efb275
MadLibberator <libbylord@hotmail.com>
2022-12-14 15:45:31 -0700

    Optimized Day14 even further: 390ms to 11.5ms

--- Puzzles/Day14/Day14.cs
+++ Puzzles/Day14/Day14.cs
@@ -10,6 +10,7 @@ public class Day14 : Puzzle
 {
     private readonly HashSet<Vector2Int> _walls = new();
     private readonly HashSet<Vector2Int> _sands = new();
+    private readonly Stack<Vector2Int> _startingPoints = new();
     private readonly Vector2Int _startingPos = new(500, 0);
     private int _maxDepth;
 
@@ -39,11 +40,12 @@ public class Day14 : Puzzle
         }
 
         _maxDepth += 2;
+        _startingPoints.Push(_startingPos);
     }
 
     public override void SolvePart1()
     {
-        while (TryGetNextPosition(_startingPos, out var settledPos))
+        while (TryGetNextPosition(_startingPoints, out var settledPos))
             _sands.Add(settledPos);
         _logger.Log(_sands.Count);
     }
@@ -53,43 +55,62 @@ public class Day14 : Puzzle
         var settledPos = Vector2Int.Zero;
         while (settledPos != _startingPos)
         {
-            _ = TryGetNextPosition(_startingPos, out settledPos);
+            _ = TryGetNextPosition(_startingPoints, out settledPos);
             _sands.Add(settledPos);
         }
         _logger.Log(_sands.Count);
     }
 
-    private bool TryGetNextPosition(Vector2Int pos, out Vector2Int settlePos)
+    private bool TryGetNextPosition(Stack<Vector2Int> startingPoints, out Vector2Int settlePos)
     {
+        var pos = startingPoints.Peek();
+        if (_sands.Contains(pos))
+        {
+            startingPoints.Pop();
+            pos = startingPoints.Peek();
+        }
+
         while (true)
         {
             settlePos = pos; // capture last known safe position to settle
             pos.Y += 1; // move down 1 step
             if (pos.Y >= _maxDepth) return false; // endless void (a.k.a. hit the floor)
-            if (!IsBlocked(pos)) continue; // can move down, keep falling
+            if (!IsBlocked(pos))
+            {
+                startingPoints.Push(pos); // can move down
+                continue;
+            }
             pos.X -= 1; // can't move down, try down-left position
-            if (!IsBlocked(pos)) continue; // can move left, keep falling
+            if (!IsBlocked(pos))
+            {
+                startingPoints.Push(pos); // can move left
+                continue;
+            }
             pos.X += 2; // can't move left, try down-right position
-            if (!IsBlocked(pos)) continue; // can move right, keep falling
+            if (!IsBlocked(pos))
+            {
+                startingPoints.Push(pos); // can move right
+                continue;
+            }
             return true; // can't move down/left/right, the sand settles
         }
+        
+        bool IsBlocked(Vector2Int pos) => _sands.Contains(pos) || _walls.Contains(pos);
     }
-
-    private bool IsBlocked(Vector2Int pos) => _sands.Contains(pos) || _walls.Contains(pos);
 
     private void DrawResults()
     {
         var sb = new StringBuilder();
         var xMin = _sands.Min(s => s.X);
         var xMax = _sands.Max(s => s.X);
-        for (int y = 0; y < _maxDepth; y++)
+        for (int y = 0; y <= _maxDepth; y++)
         {
             for (int x = xMin; x <= xMax; x++)
             {
                 var pos = new Vector2Int(x, y);
                 if (x == 500 && y == 0) sb.Append('+');
+                else if (y == _maxDepth || _walls.Contains(pos)) sb.Append('#');
                 else if (_sands.Contains(pos)) sb.Append('o');
-                else if (_walls.Contains(pos)) sb.Append('#');
                 else sb.Append('.');
             }
             sb.AppendLine();
